aa0395419819b36e321d3982696ea41b77f701d1
Falasefemi2 <femifalase228@gmail.com>
2024-03-26 13:58:36 +0100

    feat(strings,int,variable)

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
-func maim()  {
-	fmt.Println("Hello World")
+func main()  {
+// strings
+var nameOne string = "Falase Femi"
+var nameTwo = "Samuel"
+var nameThree string
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameOne = "Kemi Falase"
+nameTwo = "David Falase"
+nameThree = "Yinka Falase"
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameFour := "Ibukun"   
+fmt.Println((nameFour))
+
+// ints
+var ageOne int = 20
+var ageTwo = 30
+ageThree := 90
+
+fmt.Println(ageOne,ageTwo,ageThree)
 }
