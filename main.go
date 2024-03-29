59d8a2517d97d87865934eb72239ed336a6168c0
Falasefemi2 <femifalase228@gmail.com>
2024-03-29 11:36:21 +0100

    slices

--- main.go
+++ main.go
@@ -15,25 +15,13 @@ func main() {
 	// fmt.Println("original", greeting)
 
 
-	// ages := []int{20,302,22,11,30,990,2202}
+	ages := []int{20,302,22,11,30,990,2202}
+	fm := ages[2:5]
+	fmt.Println(fm)
 
 	// sort.Ints(ages)
 	// fmt.Println(ages)
 
-	var ages map[string]int
-	ages = make(map[string]int)
 
-	ages["femi"] = 24
-	ages["sam"] = 26
-	ages["ayo"] = 27
-
-	femiAge := ages["femi"]
-	fmt.Println(femiAge)
-
-	if age, ok := ages["sam"]; ok {
-		fmt.Println("sam age is", age)
-	} else {
-		fmt.Println("sam age is not available")
-	}
 
 }
