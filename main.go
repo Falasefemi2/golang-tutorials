dde36f9be14ede77af34b82e95450083b3608059
Falasefemi2 <femifalase228@gmail.com>
2024-03-29 11:33:47 +0100

    relearn map

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
@@ -16,8 +15,25 @@ func main() {
 	// fmt.Println("original", greeting)
 
 
-	ages := []int{20,302,22,11,30,990,2202}
+	// ages := []int{20,302,22,11,30,990,2202}
 
-	sort.Ints(ages)
-	fmt.Println(ages)
+	// sort.Ints(ages)
+	// fmt.Println(ages)
+
+	var ages map[string]int
+	ages = make(map[string]int)
+
+	ages["femi"] = 24
+	ages["sam"] = 26
+	ages["ayo"] = 27
+
+	femiAge := ages["femi"]
+	fmt.Println(femiAge)
+
+	if age, ok := ages["sam"]; ok {
+		fmt.Println("sam age is", age)
+	} else {
+		fmt.Println("sam age is not available")
+	}
+
 }
