18aa5289b96bb3017906d48ad054fac5896d4531
Falasefemi2 <femifalase228@gmail.com>
2024-04-01 21:14:27 +0100

    program to print number divisible ny 3 and 5

--- main.go
+++ main.go
@@ -5,23 +5,15 @@ import (
 )
 
 func main() {
-	var ages = [5]int{1,2,3,4,5}
-	fmt.Println(ages, len(ages))
-
-	// slices
-	var scores = []int{100,90,80}
-	fmt.Println(scores)
-	scores[2] = 70
-	fmt.Println(scores)
-	newvalue := append(scores, 60)
-	fmt.Println(newvalue)
-
-	// slices ranges
-	rangeOne :=   ages[:4]
-	fmt.Println(rangeOne)
-
-	rangeOne = append(rangeOne, 7)
-	fmt.Println(rangeOne)
-
-
+	for i := 1; i <= 100; i++ {
+		if i % 3 ==0 && i % 5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	} 
 }
