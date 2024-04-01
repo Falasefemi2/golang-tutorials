bdf7bc0e6182794afac053d150a657016330ba62
Falasefemi2 <femifalase228@gmail.com>
2024-04-01 20:59:07 +0100

    slices & arrays

--- main.go
+++ main.go
@@ -5,39 +5,23 @@ import (
 )
 
 func main() {
-	// var ages  [3]int = [3]int{1,2,3}
-	// var ages = [3]int{1,2,3}
+	var ages = [5]int{1,2,3,4,5}
+	fmt.Println(ages, len(ages))
 
-	// names := [4]string{"femi", "samuel", "dare", "ayo"}
-	// names[1] = "yinka"
-	// fmt.Println(ages, len(ages))
-	// fmt.Println(names, len(names))
+	// slices
+	var scores = []int{100,90,80}
+	fmt.Println(scores)
+	scores[2] = 70
+	fmt.Println(scores)
+	newvalue := append(scores, 60)
+	fmt.Println(newvalue)
 
-	// // slices
-	// var scores = []int{100,90,80}
-	// fmt.Println(scores)
-	// scores[2] = 34
-	// scores = append(scores, 67)
-	// fmt.Println(scores)
+	// slices ranges
+	rangeOne :=   ages[:4]
+	fmt.Println(rangeOne)
 
-	// fmt.Print("Enter your number")
-	// var input float64
-	// fmt.Scanf("%f", &input)
+	rangeOne = append(rangeOne, 7)
+	fmt.Println(rangeOne)
 
-	// output := input * 2
-	// fmt.Println(output) 
 
-	fmt.Print("Enter Fahrenheit value")
-	var temp float64
-	fmt.Scanln(&temp)
-
-	ans := (temp * 9/5) + 32
-	fmt.Println(ans) 
-
-	fmt.Print("Enter feet")
-	var feet float64
-	fmt.Scanln(&feet)
-
-	meters := feet * 0.3048
-	fmt.Println(meters)
 }
