66807e9aaf9acffdb26b3123e332bc4415dee4e0
Falasefemi2 <femifalase228@gmail.com>
2024-04-01 21:37:45 +0100

    sort packages

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-	greeting := "Hello from greetings"
+	// greeting := "Hello from greetings"
 
-	fmt.Println(strings.Contains(greeting, "Hello!"))
-	fmt.Println(strings.ReplaceAll(greeting,"Hello", "Hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "lo"))
-	fmt.Println(strings.Split(greeting, "  "))
-	fmt.Println("Original", greeting)
+	// fmt.Println(strings.Contains(greeting, "Hello!"))
+	// fmt.Println(strings.ReplaceAll(greeting,"Hello", "Hi"))
+	// fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.Index(greeting, "lo"))
+	// fmt.Println(strings.Split(greeting, "  "))
+	// fmt.Println("Original", greeting)
+
+	ages := []int{10, 20, 30, 40, 50, 60, 80, 70, 789}
+
+	sort.Ints(ages)
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 90)
+	fmt.Println(index)
+
+	names := []string{"femi","sam","ayo","dare"}
+	sort.Strings(names)
+	fmt.Println(names)
+
+	fmt.Println(sort.SearchStrings(names, "femi"))
 }
