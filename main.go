660456e1daabb5a9eef4f35f06ba44e68af36d68
Falasefemi2 <femifalase228@gmail.com>
2024-04-01 22:38:02 +0100

    for loops

--- main.go
+++ main.go
@@ -1,31 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
-	// greeting := "Hello from greetings"
+	// age := 25
 
-	// fmt.Println(strings.Contains(greeting, "Hello!"))
-	// fmt.Println(strings.ReplaceAll(greeting,"Hello", "Hi"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Index(greeting, "lo"))
-	// fmt.Println(strings.Split(greeting, "  "))
-	// fmt.Println("Original", greeting)
+	// fmt.Println(age <= 45)
+	// fmt.Println(age >= 50)
+	// fmt.Println(age == 45)
+	// fmt.Println(age != 50)
 
-	ages := []int{10, 20, 30, 40, 50, 60, 80, 70, 789}
+	// if age < 30 {
+	// 	fmt.Println("age is less than 30")
+	// } else if age < 40 {
+	// 	fmt.Println("age is less than 40")
+	// } else {
+	// 	fmt.Println("age is not less than 45")
+	// }
 
-	sort.Ints(ages)
-	fmt.Println(ages)
+	names := []string{"mario", "femi", "sam", "emma", "david"}
 
-	index := sort.SearchInts(ages, 90)
-	fmt.Println(index)
-
-	names := []string{"femi","sam","ayo","dare"}
-	sort.Strings(names)
-	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "femi"))
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pso", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("Breaking at pos", index)
+			break
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 }
+
+
