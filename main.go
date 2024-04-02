bb236ace80d5cf99b8235e647894d6f425985b45
Falasefemi2 <femifalase228@gmail.com>
2024-04-02 11:56:52 +0100

    get the highest scorer

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
-var scores = 99.5
+func highestScorer(scores map[string]int) string {
+	// Your code here
+	var highestScore int
+	var highestScorer string
 
-func main() {
-	sayHello("Femi")
-
-	for _, v := range points {
-		fmt.Println(v)
+	for name, score := range scores {
+		if score > highestScore {
+			highestScore = score
+			highestScorer = name
+		}
 	}
 
-	showScore()
+	return highestScorer
+
+
+}
+
+func main() {
+	scores := map[string]int{
+		"Alice": 85,
+		"Bob": 92,
+		"Charlie": 78,
+		"David": 95,
+	}
+	fmt.Println(highestScorer(scores)) // Should print: "David"
+
+	
 }
