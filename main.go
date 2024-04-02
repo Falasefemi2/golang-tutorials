60bd7a80976f8113c5f6f14b11bd97ad365c9a82
Falasefemi2 <femifalase228@gmail.com>
2024-04-02 12:03:35 +0100

    merge two maps together

--- main.go
+++ main.go
@@ -2,24 +2,31 @@ package main
 
 import "fmt"
 
-func totalInventoryValue(inventory map[string]int) int {
+func mergeInventories(store1, store2 map[string]int) map[string]int {
 	// Your code here
-	total := 0
-
-	for _, v :=  range inventory {
-		total += v
+	merged := make(map[string]int)
+	for item, quantity := range store1 {
+		merged[item] += quantity
 	}
-	return total
+	for item, quantity := range store2 {
+		merged[item] += quantity
+	}
+
+	return merged
+
 }
 
 func main() {
-	inventory := map[string]int{
-		"apple":  50,
-		"banana": 30,
-		"orange": 40,
+	store1 := map[string]int{
+		"apple":  10,
+		"banana": 20,
 	}
-	
-	fmt.Println(totalInventoryValue(inventory)) // Should print: 120
 
-	
+	store2 := map[string]int{
+		"banana": 15,
+		"orange": 25,
+	}
+
+	merged := mergeInventories(store1, store2)
+	fmt.Println(merged) // Should print: map[apple:10 banana:35 orange:25]
 }
