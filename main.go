1b09a6950c27a1f592312c72423b2dd2860571cf
Falasefemi2 <femifalase228@gmail.com>
2024-04-02 16:38:47 +0100

    pick the smallest number

--- main.go
+++ main.go
@@ -2,22 +2,27 @@ package main
 
 import "fmt"
 
-func updateName(x string)  {
-	x = "wedge"
+func getTheSmallest() {
+	x := []int {
+		20,5,4,1,-1,
+	}
+
+	if len(x) == 0 {
+		fmt.Println("Slice is empty")
+		return
+	}
+
+	smallest := x[0]
+	for _, num := range x {
+		fmt.Println(num)
+		if num < smallest {
+			smallest = num
+		}
+	}
+	fmt.Println("The smallest number is:", smallest)
+
 }
 
-
 func main() {
-	name := "tife"
-
-	updateName(name)
-
-	fmt.Println("memory address of name is : ", &name)
-
-	m := &name
-	fmt.Println(m)
-	fmt.Println("value at memory addresss: ", *m)
-	fmt.Println(name)
-
-
+	getTheSmallest()
 }
