2d4b33b561d316aaaa1b2b9b42a800adaadebeeb
Falasefemi2 <femifalase228@gmail.com>
2024-04-02 16:54:40 +0100

    commit to check result and boolean

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-
+func half(n int) (int, bool) {
+	if n%2 == 0 {
+		return n / 2, true // return half of n and true if n is even
+	} else {
+		return 0, false // return 0 and false if n is odd
+	}
+}
 
 func main() {
-xs := []float64{1,2,3,4,5,6,7,8,9,10}
-
-total := 0.0
-for _, v := range xs {
-	total += v
-}
-fmt.Println(total / float64(len(xs)))
+	result, isEven := half(6)
+	fmt.Println(result, isEven) // Output: 0 false
 }
