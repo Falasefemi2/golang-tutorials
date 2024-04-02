87b4fa85e296c631ae422d18338f157060d70884
Falasefemi2 <femifalase228@gmail.com>
2024-04-02 22:10:07 +0100

    struct

--- main.go
+++ main.go
@@ -2,28 +2,8 @@ package main
 
 import "fmt"
 
-func half(n int) (int, bool) {
-	if n%2 == 0 {
-		return n / 2, true // return half of n and true if n is even
-	} else {
-		return 0, false // return 0 and false if n is odd
-	}
-}
-
-func getGreatest() {
-x:= []int{20,90,30}
-greatest := x[0]
-for _, v := range x {
-if v > greatest {
-	greatest = v
-}
-}
-fmt.Println(greatest)
-
-}
-
 func main() {
-	// result, isEven := half(6)
-	// fmt.Println(result, isEven) // Output: 0 false
-	getGreatest()
+	mybill := newBill("femi's bill")
+	
+	fmt.Println(mybill)
 }
