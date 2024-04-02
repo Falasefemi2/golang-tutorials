bbf3a7bb97a78d21082d495a0da77cf28df7c9e7
Falasefemi2 <femifalase228@gmail.com>
2024-04-02 11:42:10 +0100

    package scope

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-
-func getEvenOdd(numbers []int) ([]int, []int) {
-	var evenNumber []int
-	var oddNumber []int
-
-	for _, v := range numbers {
-		if v % 2 == 0 {
-			evenNumber = append(evenNumber, v)
-		} else {
-			evenNumber = append(oddNumber, v)
-
-		}
-	}
-	return evenNumber, oddNumber
-}
+var scores = 99.5
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	even, odd := getEvenOdd(numbers)
-	fmt.Println("Even numbers:", even)
-    fmt.Println("Odd numbers:", odd)
+	sayHello("Femi")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScore()
 }
